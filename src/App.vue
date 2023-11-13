b03423a577829299ab2b016895e2be7f2f60e061
the-man2004 <heinrichhb111@gmail.com>
2023-11-13 15:18:33 -0800

    Started testing data fetching.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,45 @@
 <template>
-  <h1>Hello world</h1>
+  <button @click="fetchNews">FETCH NEWS</button>
 </template>
 
-<script setup></script>
+<script setup>
+// Get the date
+const getDateString = () => {
+  // DATE
+  const currentDate = new Date();
+
+  // 7 days ago
+  const sevenDaysAgo = new Date(currentDate);
+  sevenDaysAgo.setDate(currentDate.getDate() - 7);
+
+  const year = sevenDaysAgo.getFullYear();
+  const month = ("0" + (sevenDaysAgo.getMonth() + 1)).slice(-2);
+  const day = ("0" + sevenDaysAgo.getDate()).slice(-2);
+
+  const date = year + "-" + month + "-" + day;
+
+  return date;
+};
+
+// fetchimg the data
+const fetchNews = async () => {
+  // Date
+  const date = getDateString();
+
+  // URL
+  const url =
+    "https://newsapi.org/v2/everything?" +
+    "q=apple&" +
+    `from=${date}&` +
+    "pageSize=20&" +
+    "sortBy=popularity&" +
+    "apiKey=37d4f2f323884e05887061669ba9133c";
+  const response = await fetch(url);
+
+  const responseData = await response.json();
+  console.log(responseData);
+};
+</script>
 
 <style>
 #app {
