52c141d903daf3cefd2e26485e0a5a1c6aa8f0a1
the-man2004 <heinrichhb111@gmail.com>
2023-11-14 14:43:05 -0800

    Added Search & About views.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
+import About from "../views/About";
 import Home from "../views/Home";
+import Search from "../views/Search";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/search",
+    name: "Search",
+    component: Search,
   },
 ];
 
