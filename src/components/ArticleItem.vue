5b7d37283c160269121a11e24f3dd0e1d633628e
the-man2004 <heinrichhb111@gmail.com>
2023-11-14 16:40:01 -0800

    Now outputting all other articles in ArticleList.

--- src/components/ArticleItem.vue
+++ src/components/ArticleItem.vue
@@ -1,3 +1,21 @@
 <template>
-  <h3>Article item</h3>
+  <div>
+    <img :src="thumbnail" alt="" />
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <span>{{ author }}</span>
+    <a :href="link">Read more</a>
+  </div>
 </template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["article"]);
+
+const thumbnail = computed(() => props.article.urlToImage);
+const title = computed(() => props.article.title);
+const description = computed(() => props.article.description);
+const author = computed(() => props.article.author);
+const link = computed(() => props.article.url);
+</script>
--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -1,9 +1,16 @@
 <template>
   <first-article />
-  <article-item />
+  <article-item
+    v-for="(article, index) in newsStore.otherArticles"
+    :key="index"
+    :article="article"
+  />
 </template>
 
 <script setup>
 import FirstArticle from "./FirstArticle.vue";
 import ArticleItem from "./ArticleItem.vue";
+import { useNewsStore } from "../stores/useNewsStore";
+
+const newsStore = useNewsStore();
 </script>
