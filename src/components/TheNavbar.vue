411455660389a4e862e62976be27652101828a6c
the-man2004 <heinrichhb111@gmail.com>
2023-11-16 16:42:34 -0800

    Added a dropdown menu for mobile devices.

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,26 +1,11 @@
 <template>
   <nav
-    class="flex flex-row justify-between shadow-lg py-1 px-4 md:py-2 md:px-8"
+    class="flex flex-row justify-between shadow-lg py-1 px-4 md:py-2 sm:px-8"
   >
     <div>
       <router-link to="/" class="font-bold md:text-xl">Daily News</router-link>
     </div>
-    <div class="lg:hidden">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6 cursor-pointer"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-        />
-      </svg>
-    </div>
+    <the-menu></the-menu>
     <div class="hidden lg:flex lg:gap-6">
       <button>tech</button>
       <button>health</button>
@@ -37,6 +22,10 @@
     </div>
   </nav>
 </template>
+
+<script setup>
+import TheMenu from "../components/burgerMenu/TheMenu.vue";
+</script>
 
 <style scoped>
 button:hover {
---
+++ src/components/burgerMenu/TheMenu.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="lg:hidden">
+    <svg
+      @click="handleMenuClick"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6 cursor-pointer"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+    <div
+      v-if="isMenuOpen"
+      class="p-4 shadow-xl absolute top-8 left-0 right-0 gap-4 bg-white sm:p-8"
+    >
+      <button>tech</button>
+      <button>health</button>
+      <button>sports</button>
+      <button>science</button>
+      <button>general</button>
+      <button>business</button>
+      <button>entertainment</button>
+      <router-link
+        to="/about"
+        class="font-semibold my-auto px-4 border-2 border-black"
+        >about</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const isMenuOpen = ref(false);
+
+const handleMenuClick = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+</script>
+
+<style scoped>
+button {
+  display: block;
+  padding-bottom: 10px;
+}
+
+button:hover {
+  text-decoration: underline;
+}
+</style>
