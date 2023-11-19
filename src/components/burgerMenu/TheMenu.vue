8750dbaaa3f8da9ad25a3cee2b91c00590f65e89
the-man2004 <heinrichhb111@gmail.com>
2023-11-19 12:22:30 -0800

    Added search icon in Navbar.

--- src/components/burgerMenu/TheMenu.vue
+++ src/components/burgerMenu/TheMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="lg:hidden">
+  <nav class="lg:hidden">
     <svg
       @click="toggleMenu"
       xmlns="http://www.w3.org/2000/svg"
@@ -19,6 +19,23 @@
       v-if="isMenuOpen"
       class="p-4 shadow-xl absolute top-8 left-0 right-0 gap-4 bg-white sm:p-8"
     >
+      <router-link to="/search">
+        <svg
+          @click="handleBtnClick"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 mb-4"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </router-link>
       <button @click="handleBtnClick">tech</button>
       <button @click="handleBtnClick">health</button>
       <button @click="handleBtnClick">sports</button>
@@ -26,6 +43,7 @@
       <button @click="handleBtnClick">general</button>
       <button @click="handleBtnClick">business</button>
       <button @click="handleBtnClick">entertainment</button>
+
       <router-link
         to="/about"
         @click="toggleMenu"
@@ -33,7 +51,7 @@
         >about</router-link
       >
     </div>
-  </div>
+  </nav>
 </template>
 
 <script setup>
