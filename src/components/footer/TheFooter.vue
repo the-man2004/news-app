426d8c54c09310b8c88dba356f31c8b13a970e43
the-man2004 <heinrichhb111@gmail.com>
2023-11-20 15:35:32 -0800

    Fixed spacing issue in footer.

--- src/components/footer/TheFooter.vue
+++ src/components/footer/TheFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-black text-white mt-12 mt:pt-20">
     <div
-      class="max-w-6xl mx-auto text-xs p-4 md:grid md:grid-cols-5 md:text-sm md:p-8"
+      class="max-w-6xl mx-auto text-xs p-4 md:grid md:grid-cols-4 md:text-sm md:p-8"
     >
       <div class="mb-8 text-center md:mb-0 md:col-span-2 md:text-left">
         <router-link to="/technology" class="block text-lg font-bold"
